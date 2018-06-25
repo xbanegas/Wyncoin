a8a7f4d27478a14ba05c1b75b502293c467398fa
xbanegas <xbanegas@gmail.com>
2018-06-24 20:29:05 -0400

    first marker

--- client/src/component/Map.js
+++ client/src/component/Map.js
@@ -7,15 +7,45 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNkdWVuYXMiLCJhIjoiY2ppczBhNnVkMXMzbDN3c
 export default class Map extends Component {
 
 	componentDidMount() {
+		let geoLoc
 		navigator.geolocation.getCurrentPosition((position) => {
-			let geoLoc = [position.coords.longitude, position.coords.latitude];
+			geoLoc = [position.coords.longitude, position.coords.latitude];
 			let map = new mapboxgl.Map({
 				container: this.mapContainer,
 				style: 'mapbox://styles/mapbox/streets-v9',
 				center: geoLoc,
 				zoom: 14
 			});
+			// [-77.032, 38.913]
+			var geojson = {
+				type: 'FeatureCollection',
+				features: [{
+					type: 'Feature',
+					geometry: {
+						type: 'Point',
+						coordinates: geoLoc
+					},
+					properties: {
+						title: 'Mapbox',
+						description: 'Washington, D.C.'
+					}
+				}]
+			};
+			// add markers to map
+			geojson.features.forEach(function(marker) {
+
+				// create a HTML element for each feature
+				var el = document.createElement('div');
+				el.className = 'marker';
+
+				// make a marker for each feature and add to the map
+				new mapboxgl.Marker(el)
+				.setLngLat(marker.geometry.coordinates)
+				.addTo(map);
+			});
 		});
+
+
 	}
 
 	render() {
