ace3ed8e5c2454afc6691d909e0a70738e429b32
xbanegas <xbanegas@gmail.com>
2018-06-26 00:25:26 -0400

    first popups

--- client/src/utils/mapUtils.js
+++ client/src/utils/mapUtils.js
@@ -29,6 +29,8 @@ const initMap = async(mapContainer) =>{
       // make a marker for each feature and add to the map
       new mapboxgl.Marker(el)
         .setLngLat(marker.geometry.coordinates)
+        .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
         .addTo(map);
     });
   });
