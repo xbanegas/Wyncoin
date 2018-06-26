14efbe8d7418dce00caef87c46f0c78796795e44
xbanegas <xbanegas@gmail.com>
2018-06-26 04:16:43 -0400

    popup font color fix

--- client/src/component/MapPopup.js
+++ client/src/component/MapPopup.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Map extends Component {
   render(){
     return(
-      <div>
+      <div className="map-popup">
         <h3>{this.props.vendor.properties.title}</h3>
         <p>{this.props.vendor.properties.category}</p>
         <button onClick={this.props.handleDirectionClick}>Go</button>
