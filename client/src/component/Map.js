1acd55746a652471f92a06b8938d931ff9159931
xbanegas <xbanegas@gmail.com>
2018-06-26 00:16:18 -0400

    vendor and map move to utils and map styling

--- client/src/component/Map.js
+++ client/src/component/Map.js
@@ -1,49 +1,11 @@
 import React, { Component } from 'react';
-import mapboxgl from 'mapbox-gl';
-import Sidebar from './Sidebar';
-import axios from 'axios';
+import {initMap} from '../utils/mapUtils';
 import '../css/map.css';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiY2hyaXNkdWVuYXMiLCJhIjoiY2ppczBhNnVkMXMzbDN3cDhzczlmbTE3ayJ9.6YUyaCiEPJ_0b3QcoZxk5w';
 
 export default class Map extends Component {
-
-	getVendors = async (geoLoc) => {
-		return await axios.get(`/vendors?long=${geoLoc[0]}&lat=${geoLoc[1]}`)
-	}
-
 	componentDidMount() {
-
-		let geoLoc;
-		navigator.geolocation.getCurrentPosition(async (position) => {
-			geoLoc = [position.coords.longitude, position.coords.latitude];
-			let map = new mapboxgl.Map({
-				container: this.mapContainer,
-				style: 'mapbox://styles/mapbox/streets-v9',
-				center: geoLoc,
-				zoom: 12
-			});
-
-			console.log('geojson');
-			let res = await this.getVendors(geoLoc);
-
-			let geojson = res.data;
-			console.log(geojson.features);
-
-			// add markers to map
-			geojson.features.forEach(function (marker) {
-				// create a HTML element for each feature
-				var el = document.createElement('div');
-				el.className = 'marker';
-
-				// make a marker for each feature and add to the map
-				new mapboxgl.Marker(el)
-					.setLngLat(marker.geometry.coordinates)
-					.addTo(map);
-			});
-		});
-
-
+		initMap(this.mapContainer);
 	}
 
 	render() {
@@ -54,9 +16,11 @@ export default class Map extends Component {
 			width: '100%'
 		};
 		return (
-			<div>
-				<div id="mapContainer" style={{ position: "relative", }}><div style={style} ref={el => this.mapContainer = el} /></div>)
-			</div>
+
+				<div id="mapContainer" style={{ position: "relative", }}>
+					<div style={style} ref={el => this.mapContainer = el} />
+				</div>
+
 		)
 	}
 }
---
+++ utils/vendors.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const Features = require('./featureCollection');
+
+const getCoinmapVendors = async (query) => {
+  let radius = 2;
+  let lat1 = query.lat - radius;
+  let lat2 =  query.lat + radius;
+  let lon1 = query.long - radius;
+  let lon2 = query.long + radius;
+  let coinmapParams = `?lat1=${lat1}&lat2=${lat2}&lon1=${lon1}&lon2=${lon2}`;
+  let coinmapUrl = "https://coinmap.org/api/v1/venues/";
+  console.log(coinmapUrl + coinmapParams);
+  let res = await axios.get(coinmapUrl + coinmapParams);
+  return res.data.venues
+}
+
+const genFeatureCollection =  async (query)=> {
+  let featureCollection = new Features.FeatureCollection();
+  let currentLoc = new Features.Feature([query.long, query.lat], 'Current');
+  featureCollection.addFeature(currentLoc);
+  let coinmapRes =  await getCoinmapVendors(query);
+  coinmapRes.forEach((venue)=>{
+    let venueLoc = [String(venue.lon), String(venue.lat)];
+    let newVenue = new Features.Feature(venueLoc, venue.name);
+    featureCollection.addFeature(newVenue)
+  });
+  return featureCollection
+}
+
+module.exports.genFeatureCollection = genFeatureCollection;
